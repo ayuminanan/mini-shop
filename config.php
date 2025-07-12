bda1fe474fafe9686bcb1e585c482963ec570d0c
ayumi <j597512718@gmail.com>
2025-07-12 23:17:52 +0900

    配置数据库连接，修改 config.php 使用虚拟机IP连接MySQL

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 // config.php
 
-define('DB_HOST', '127.0.0.1');
+define('DB_HOST', '192.168.4.128');
 define('DB_USER', 'mini_user');
 define('DB_PASS', 'ab960204');
 define('DB_NAME', 'mini_shop');
