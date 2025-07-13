792facd6b98401c01399ef14d1388209f1686d30
ayumi <j597512718@gmail.com>
2025-07-14 00:39:48 +0900

    fix: update config.php to use mini_shop database

--- config.php
+++ config.php
@@ -5,7 +5,7 @@ define('DB_HOST', 'hopper.proxy.rlwy.net'); // RAILWAY_TCP_PROXY_DOMAIN 的值
 define('DB_PORT', 21728);                   // RAILWAY_TCP_PROXY_PORT 的值（需查看 Railway 控制台的 Connect 页）
 define('DB_USER', 'root');
 define('DB_PASS', 'xIOSANZNTFGLAuaXXesijWQsbLiDiTmO');
-define('DB_NAME', 'railway');
+define('DB_NAME', 'mini_shop');
 
 $con = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME, DB_PORT);
 
